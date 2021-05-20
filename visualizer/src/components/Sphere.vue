b916b2ce628214a64dc5a4f64d5233028c27e3d0
Thomas Eykamp <embergameslp@gmail.com>
2021-05-20 17:18:37 -0400

    Basic curves through points

--- visualizer/src/components/Sphere.vue
+++ visualizer/src/components/Sphere.vue
@@ -1,50 +1,68 @@
 <template>
   <div>
-    
+    <canvas id="canvas"/>
   </div>
 </template>
 
 <script>
-import * as THREE from 'three'
-
 export default {
   name: 'Sphere',
   data() {
     return {
-      sphere: null,
-      renderer: null,
-      scene: null,
-      camera: null
+      ctx: null,
+      height: 0,
+      width: 0,
+      pointList: [],
     }
   },
   methods: {
+    randomNumber(min, max) {  
+      return Math.random() * (max - min) + min; 
+    },
+
+    generatePoints(num) {
+      const bound = 10;
+      for (let i = 0; i < num; i++) {
+        this.pointList.push([this.randomNumber(bound, this.width-bound), this.randomNumber(bound, this.height-bound)])
+      }
+    },
+
     init() {
-      this.scene = new THREE.Scene();
-      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(window.innerWidth-10, window.innerHeight-20);
-      document.body.appendChild(this.renderer.domElement);
-
-      const geometry = new THREE.SphereGeometry(5, 15, 15);
-      const material = new THREE.PointsMaterial({ 
-          size: 0.03 
-        });
-      this.sphere = new THREE.Points(geometry, material);
-      this.scene.add(this.sphere);
-
-      this.camera.position.z = 25;
+      var canvas = document.getElementById("canvas");
+      this.ctx = canvas.getContext("2d");
+      canvas.height = window.innerHeight-20;
+      this.height = canvas.height;
+      canvas.width = innerWidth-20;
+      this.width = canvas.width
+      this.ctx.lineWidth = 15;
+      this.generatePoints(10);
+      
     },
 
     animate() {
       requestAnimationFrame(this.animate);
+      this.ctx.clearRect(0, 0, this.width, this.height);
+      // for (let i = 0; i < this.pointList.length; i++) {
+      //   this.ctx.beginPath();
+      //   this.ctx.arc(this.pointList[i][0], this.pointList[i][1], 5, 0, Math.PI * 2, false);
+      //   this.ctx.fillStyle = "black";
+      //   this.ctx.fill();
+      // }
 
-      this.sphere.rotation.x += 0.01;
-      this.sphere.rotation.y += 0.01;
-
-      this.renderer.render(this.scene, this.camera);
+      this.ctx.moveTo(this.pointList[0][0], this.pointList[0][1]);
+      
+      for(let i = 0; i < this.pointList.length-1; i++) {
+        const x_mid = (this.pointList[i][0] + this.pointList[i+1][0]) / 2;
+        const y_mid = (this.pointList[i][1] + this.pointList[i+1][1]) / 2;
+        const cp_x1 = (x_mid + this.pointList[i][0]) / 2;
+        const cp_x2 = (x_mid + this.pointList[i+1][0]) / 2;
+        this.ctx.quadraticCurveTo(cp_x1, this.pointList[i][1], x_mid, y_mid);
+        this.ctx.quadraticCurveTo(cp_x2, this.pointList[i+1][1], this.pointList[i+1][0], this.pointList[i+1][1]);
+      }
+      this.ctx.stroke();
     }
   },
+
   mounted() {
     this.init();
     this.animate();
@@ -53,6 +71,8 @@ export default {
 </script>
 
 <style scoped>
-
+#canvas {
+  background: lightgoldenrodyellow;
+}
 
 </style>
